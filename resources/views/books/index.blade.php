467a6cf649c8626e89d06a7192071883fce8d755
DUAN Zhiyan <dzy0451@gmail.com>
2016-08-21 13:51:58 +0800

    minor: small tweak to looks

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -2,12 +2,14 @@
 
 @section('content')
 
-    @foreach($books as $book)
-
+    @foreach($books->chunk(4) as $chunk)
+        @foreach($chunk as $book)
         <div class="card">
             <img class="card-img-top" src="{{ asset($book->cover) }}" alt="{{ $book->title }}">
             <div class="card-block">
-                <h4 class="card-title">{{ $book->title }}</h4>
+                <h4 class="card-title">
+                    <a href="{{ route('books.show', $book->id) }}">{{ $book->title }}</a>
+                </h4>
                 <p class="card-text">{{ $book->description }}</p>
                 <a href="#" class="btn btn-primary">
                     <span class="fa fa-inbox"></span>
@@ -15,7 +17,7 @@
                 </a>
             </div>
         </div>
-
+        @endforeach
     @endforeach
 
 @stop
