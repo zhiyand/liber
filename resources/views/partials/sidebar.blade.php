48831b4b42b4255e7faf0e4ca37db2c95c8f33cd
DUAN Zhiyan <dzy0451@gmail.com>
2016-08-22 13:47:12 +0800

    add predis library; show search box to guests

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -7,20 +7,20 @@
     </div>
 @endif
 
-@if($me)
-    <div class="Widget">
-        <form action="/books/search">
-            <div class="input-group">
-                <input type="text" name="q" placeholder="Search for..." class="form-control">
-                <span class="input-group-btn">
-                    <button class="btn btn-secondary" type="submit" id="btn-search">
-                        <span class="fa fa-search"></span>
-                    </button>
-                </span>
-            </div>
-        </form>
+<div class="Widget Search">
+    <form action="/books/search">
+        <div class="input-group">
+            <input type="text" name="q" placeholder="Search for..." class="form-control">
+            <span class="input-group-btn">
+                <button class="btn btn-secondary" type="submit" id="btn-search">
+                    <span class="fa fa-search"></span>
+                </button>
+            </span>
+        </div>
+    </form>
+</div>
 
-    </div>
+@if($me)
     <div class="List Widget">
 
         <h3 class="List__title">My Book Loans</h3>
